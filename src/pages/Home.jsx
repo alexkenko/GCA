c05c3fd0806001f541b2a8b09a07f572616cf3de
alexkenko <kenkadzealex@gmail.com>
2025-12-05 17:50:08 +0400

    Remove Santa component from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,12 +45,6 @@ const Home = () => {
           </div>
         ))}
       </div>
-      
-      {/* Animated Santa */}
-      <div className="santa-container">
-        <div className="santa">🎅</div>
-      </div>
-
       <section className="hero">
         <div className="wave-container">
           <div className="wave"></div>
